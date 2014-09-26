401e4a47312abbb89366765189a0ba2c91eb96ba
root <root@localhost.localdomain>
2014-09-26 09:08:11 +0000

    add public

---
+++ public/Test/Case/AllTestsTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *suite
+ *
+ *@return void
+ */
+class AllTestsTest extends CakeTestSuite{
+	public static function suite(){	
+		$suite = new CakeTestSuite('All tests');
+		$suite->addTestDirectoryRecursive(TESTS . 'Case');
+		return $suite;
+	}
+}
+?>
